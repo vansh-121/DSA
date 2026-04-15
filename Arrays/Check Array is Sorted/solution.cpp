8012dcacb4ef432ae4ce1ebade595c574c78a4c7
Vansh <vansh.sethi98760@gmail.com>
2026-04-16 02:22:04 +0530

    problem: implement array sorting check

---
+++ Arrays/Check Array is Sorted/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check_array(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] >= arr[i - 1])
+        {
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << boolalpha << check_array(arr, n);
+}
